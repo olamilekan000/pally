6cc876037c4f759c6a3699f1be248032517335f7
Ola <olarpringo@gmail.com>
2018-05-22 08:30:12 +0100

    initial commit 2

---
+++ public/script/script.js
@@ -0,0 +1,1 @@
+ setTimeout(() => { window.location = "/menu"; }, 5000)
--- routes/routes.js
+++ routes/routes.js
@@ -1,7 +1,12 @@
+
 module.exports = (app) => {
 
 	app.get('/', (req, res) => {
 		res.render('screen');
+	})
+
+	app.get('/menu', (req, res) => {
+		res.render('menu');
 	})
 
 }
