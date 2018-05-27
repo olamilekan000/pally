3528a886212108830cf0694212c0cad34194916b
Ola <olarpringo@gmail.com>
2018-05-27 16:30:59 +0100

    added page to choose 2

--- public/script/script.js
+++ public/script/script.js
@@ -2,7 +2,7 @@ function toggleSidebar(){
 	document.getElementById('menu').classList.toggle('active');
 }
 
-//takes user to menu
-// change = () => {
-// 	window.location = "/select";
-// }
+//takes user to select
+change = () => {
+	window.location = "/select";
+}
